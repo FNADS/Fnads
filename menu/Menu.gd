13c803f2287e27832ff35b291b604e4b1072486b
KalinuxYT <kalinuxburner@gmail.com>
2024-07-30 04:55:01 -0400

    Intro splash screen fix

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -1,8 +1,12 @@
 extends Control
 
 func _ready() -> void:
+	Splash.connect("on_transition_finished", Callable(self, "_on_splash_finished"))
 	Splash.transition()
-	
+
+func _on_splash_finished():
+	print("Splash finished")
+	# Perform any additional setup needed after the splash screen finishes
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://menu/Menu.tscn")
--- menu/Splash.gd
+++ menu/Splash.gd
@@ -5,14 +5,22 @@ signal on_transition_finished
 @onready var black_screen = $Black_Screen
 @onready var splash_fade = $Splash_Fade
 
+var has_played = false  # Flag to track if the splash screen has been played
+
 func _ready():
 	black_screen.visible = false
 	splash_fade.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished(_anim):
-	self.queue_free()
+	emit_signal("on_transition_finished")
+	hide()
 	
 
 func transition():
-	black_screen.visible = true
-	splash_fade.play("Splash_Animation")
+	if has_played:
+		emit_signal("on_transition_finished")
+	else:
+		has_played = true
+		black_screen.visible = true
+		splash_fade.play("Splash_Animation")
+		show()
