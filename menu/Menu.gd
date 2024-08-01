f4636ce8db62bb28f4bda59572f3b687644a992c
KalinuxYT <KalinuxBurner@gmail.com>
2024-08-01 19:27:11 -0400

    Revert "added a tv-static overlay, simplified splash screens and menu a bit"

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -1,15 +1,19 @@
-extends CanvasLayer
+extends Control
+
+func _ready() -> void:
+	Splash.connect("on_transition_finished", Callable(self, "_on_splash_finished"))
+	Splash.transition()
 
 func _on_splash_finished():
 	print("Splash finished")
 # Perform any additional setup needed after the splash screen finishes
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://menu/menu.tscn")
+	get_tree().change_scene_to_file("res://menu/Menu.tscn")
 
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://menu/settings/options_menu.tscn")
+	get_tree().change_scene_to_file("res://menu/settings/Options_Menu.tscn")
 
 
 func _on_credits_pressed():
@@ -18,5 +22,3 @@ func _on_credits_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
-	
-	
--- menu/settings/options_menu.gd
+++
@@ -1,5 +0,0 @@
-extends CanvasLayer
-
-# Function to return to the main menu screen
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://menu/menu.tscn")
