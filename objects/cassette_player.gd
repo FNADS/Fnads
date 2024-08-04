8b43b52d8ddae4adb1e40ca4577f72f64730d1d1
HeDeAn <tannenbaumimjuli@gmail.com>
2024-08-05 01:19:56 +0200

    fixed typos

--- objects/cassette_player.gd
+++ objects/cassette_player.gd
@@ -4,8 +4,8 @@ extends Node2D
 const rewind_time : float = 5.0; # The it needs to rewind the whole tape
 
 
-@onready var music := $CassettMusic as AudioStreamPlayer2D;
-@onready var buttons := $CassettPlayerButtons as AudioStreamPlayer2D;
+@onready var music := $CassetteMusic as AudioStreamPlayer2D;
+@onready var buttons := $CassettePlayerButtons as AudioStreamPlayer2D;
 var music_time : float = 0.0;
 var rewind_step : float; # The time it rewinds each second
 var rewind_tape : bool = false;
@@ -14,7 +14,7 @@ var hover_over := NONE;
 
 enum {
 	NONE,
-	CASSETT,
+	CASSETTE,
 	REWIND,
 	STOP,
 	PLAY,
@@ -37,13 +37,13 @@ func _process(delta):
  
 
 func  _input(event) -> void:
-	if Input.is_action_just_pressed("LEFT_CLICK"): use_cassett_player();
-	if Input.is_action_just_released("LEFT_CLICK"): stop_use_cassett_player();
+	if Input.is_action_just_pressed("LEFT_CLICK"): use_cassette_player();
+	if Input.is_action_just_released("LEFT_CLICK"): stop_use_cassette_player();
 
 
-func use_cassett_player() -> void:
+func use_cassette_player() -> void:
 	match hover_over:
-		CASSETT: pass
+		CASSETTE: pass
 		REWIND:
 			if !music.playing:
 				buttons.play();
@@ -58,13 +58,13 @@ func use_cassett_player() -> void:
 				music.play(music_time);
 
 
-func stop_use_cassett_player() -> void:
+func stop_use_cassette_player() -> void:
 	if hover_over == REWIND:
 		rewind_tape = false;
 		
 
 
-func _on_cassett_enter() -> void: hover_over = CASSETT;
+func _on_cassette_enter() -> void: hover_over = CASSETTE;
 
 func _on_rewind_enter() -> void: hover_over = REWIND;
 
@@ -73,5 +73,5 @@ func _on_stop_enter() -> void: hover_over = STOP;
 func _on_play_enter() -> void: hover_over = PLAY;
 
 func _on_mouse_exit_any() -> void:
-	stop_use_cassett_player();
+	stop_use_cassette_player();
 	hover_over = NONE;
