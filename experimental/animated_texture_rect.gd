32dd6ce7f97037f2402eebe42c05485e73e903d5
HeDeAn <tannenbaumimjuli@gmail.com>
2024-08-10 00:55:14 +0200

    fixed AnimatedTextureRect

--- experimental/animated_texture_rect.gd
+++ experimental/animated_texture_rect.gd
@@ -9,11 +9,19 @@ class_name  AnimatedTextureRect extends TextureRect
 @export var animation : String = "default":
 	get: return animation;
 	set(value):
+		if !sprite_frames.has_animation(value):
+			printerr("SpriteFrame does not have the given animation!");
+			return;
 		animation = value;
+		frame = 0;
 		texture = sprite_frames.get_frame_texture(animation, frame);
 @export var frame : int = 0:
 	get: return frame;
 	set(value):
+		if sprite_frames.get_frame_count(animation) < 1:
+			printerr("SpriteFrame does not have any frames");
+			return;
+		frame = value;
 		if frame >= sprite_frames.get_frame_count(animation):
 			if !looping: stop();
 			frame = 0;
@@ -39,13 +47,14 @@ func _ready() -> void:
 
 func _process(delta) -> void:
 	if sprite_frames != null && playing:
-		assert(sprite_frames.has_animation(animation));
 		frame_delta += speed_scale * delta;
 		if frame_delta >= refresh_rate / fps: next_frame();
 
 
 func next_frame() -> void:
-	frame += 1;
+	print("Frame pre: ", frame);
+	frame = frame + 1;
+	print("Frame post: ", frame);
 	refresh_rate = sprite_frames.get_frame_duration(animation, frame);
 	frame_delta = 0;
 
