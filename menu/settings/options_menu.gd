69beebbf91ec08f69e4bbd00f0a84d9e7b5d46ee
HeDeAn <105547153+HeDeAnTheonlyone@users.noreply.github.com>
2024-08-12 19:08:08 +0200

    only left thenecessary files

--- menu/settings/options_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-# Function to return to the main menu screen
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://menu/menu.tscn")
-func _process(_delta):
-	$Background.set_size(DisplayServer.window_get_size() * 2)
