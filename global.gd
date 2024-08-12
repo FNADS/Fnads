b68c7c54092c64dcf64c90b1dd3ec8e1b38745ea
HeDeAn <tannenbaumimjuli@gmail.com>
2024-08-12 23:20:18 +0200

    .

--- global.gd
+++ global.gd
@@ -1,14 +1,6 @@
 extends Node
 
 
-
-var current_night: = 1 # current night, defaults to wan
-var current_am: int # current am, doesn't need to default to anything 
-@export var selected_cam:= 0
-var previous_cam: int
-
-
-
 var load_save_manager : LoadSaveManager = preload("res://manager/load_save_manager.gd").new();
 var display_manager : DisplayManager = preload("res://manager/display_manager.gd").new();
 var time_manager : TimeManager = preload("res://manager/time_manager.gd").new();
@@ -29,7 +21,7 @@ func _ready() -> void:
 	
 ## Sets the settings dict to the default values
 func init_default_settings() -> void:
-	var default_settings : Dictionary = {
+	var default_settings: Dictionary = {
 		"show_splash_screen": true,
 		"window_mode": 1,
 		"volume": [],
@@ -41,4 +33,6 @@ func init_default_settings() -> void:
 
 ## Sets the game_state dict to the default values
 func init_default_game_state():
-	game_state = {};
+	game_state = {
+		"unlocked_nights": 1, 
+	};
