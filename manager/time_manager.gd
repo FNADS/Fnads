34041837b6de15cbd53402e292fd123f5d7d1200
HeDeAn <tannenbaumimjuli@gmail.com>
2024-08-13 22:14:06 +0200

    Fixed time manager and added cerber passification (cassette player/headpat)

--- manager/time_manager.gd
+++ manager/time_manager.gd
@@ -20,18 +20,15 @@ var time_per_night: float;
 signal time_running_changed(is_running);
 
 
-func _ready() -> void:
+func _init() -> void:
 	time_per_night = default_time_per_nigth * variation;
 	time_per_hour = time_per_night / 6;
 
 
-func _process(delta): #run every game tick
-	if is_running: #if time is started
-		time_elapsed += delta #update time elapsed
-	
-	if gethour() >= 8: #if its 8am, end night
-		is_running = false #stop time
-		
+func process(delta) -> void:
+	if is_running: time_elapsed += delta;
+
+
 #region This does not belong in this skript
 		#if gethour() == 8: #if the current time is 8am
 			#Global.current_night += 1 # unlock next night
@@ -41,4 +38,7 @@ func _process(delta): #run every game tick
 
 
 ## Returns the current in game hour
-func gethour(): return floor(time_elapsed / time_per_hour) + 2;
+func gethour() -> int: return floori(time_elapsed / time_per_hour) + 2;
+
+
+func hours_since_timestamp(timestamp: float) -> float: return (time_elapsed - timestamp) / time_per_hour;
