8fb584a649efaed11ec1c4bb27855b3a53ca44c2
Mason <Masonmcclintic@gmail.com>
2024-10-14 20:35:45 -0400

    basic idea of what i think this needs to be

--- game/game.gd
+++ game/game.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+@export var generator_power = 100
 signal on_power_change(new_power_state: bool)
 
 func _ready():
 	break_power(3)
-
+	
 func break_power(time: int):
 	await get_tree().create_timer(time).timeout
 	var breakers = get_tree().get_first_node_in_group('Breakers')
 	breakers.break_power(6)
 	# break_power(60)
+
+func use_generator_power(power: int):
+	generator_power -= power
+	on_power_change.emit(generator_power)
+	if generator_power > 0:
+		on_power_change.emit(false)
